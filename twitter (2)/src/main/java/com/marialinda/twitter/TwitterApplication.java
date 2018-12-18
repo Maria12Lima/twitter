3b17d020adce31ca759064f9211d24ea6214319a
Maria12Lima <marialima.ufal@gmail.com>
2018-12-18 08:39:49 -0300

    Começando o projeto!

---
+++ twitter (2)/src/main/java/com/marialinda/twitter/TwitterApplication.java
@@ -0,0 +1,14 @@
+package com.marialinda.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterApplication.class, args);
+	}
+
+}
+
